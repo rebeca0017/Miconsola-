43f13bba7f0d34bf7e17dac51e8a58beb2d24150
rebeca0017 <47639885+rebeca0017@users.noreply.github.com>
2021-06-16 18:28:15 -0500

    Add files via upload

---
+++ MiPrimerConsola/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MiPrimerConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo!");
+        }
+    }
+}
